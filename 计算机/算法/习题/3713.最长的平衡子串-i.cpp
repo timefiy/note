7724a51ef3b2a22c32dee3c8b891cc70b084bcba
timefiy <kw0501199@gmail.com>
2026-02-12 22:42:18 +0800

    vault backup: 2026-02-12 22:42:17
    Affected files:
    .obsidian/app.json
    计算机/算法/习题/3713.最长的平衡子串-i.cpp
    计算机/算法/数据结构/树/树状数组.md

---
+++ 计算机/算法/习题/3713.最长的平衡子串-i.cpp
@@ -0,0 +1,20 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=3713 lang=cpp
+ *
+ * [3713] 最长的平衡子串 I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int longestBalanced(string s) {
+        for(int i = 0; i < string.size(); i++){
+
+        }
+    }
+};
+// @lc code=end
+
