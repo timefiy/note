1a74a0cabb1d9198c28b8612f6281f67c0257852
timefiy <kw0501199@gmail.com>
2026-02-13 22:34:26 +0800

    vault backup: 2026-02-13 22:34:25
    Affected files:
    计算机/算法/习题/.vscode/settings.json
    计算机/算法/习题/3714.最长的平衡子串-ii.cpp

---
+++ 计算机/算法/习题/3714.最长的平衡子串-ii.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=3714 lang=cpp
+ *
+ * [3714] 最长的平衡子串 II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int longestBalanced(string s) {
+        
+    }
+};
+// @lc code=end
+
