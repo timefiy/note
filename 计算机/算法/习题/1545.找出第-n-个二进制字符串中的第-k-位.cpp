edffbb00836d752db3468b4b87e9c30541d03281
timefiy <kw0501199@gmail.com>
2026-03-03 22:11:25 +0800

    vault backup: 2026-03-03 22:11:24
    Affected files:
    计算机/算法/习题/1545.找出第-n-个二进制字符串中的第-k-位.cpp

--- 计算机/算法/习题/1545.找出第-n-个二进制字符串中的第-k-位.cpp
+++ 计算机/算法/习题/1545.找出第-n-个二进制字符串中的第-k-位.cpp
@@ -1,3 +1,6 @@
+# include <bits/stdc++.h>
+using namespace std;
+
 /*
  * @lc app=leetcode.cn id=1545 lang=cpp
  *
@@ -8,7 +11,11 @@
 class Solution {
 public:
     char findKthBit(int n, int k) {
-        
+        string s = "0";
+    }
+
+    char findkKthString(int n, string &s){
+
     }
 };
 // @lc code=end
