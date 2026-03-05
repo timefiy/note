d4daeacc6b7264591ed320bf2f1ba195ea9d94ee
timefiy <kw0501199@gmail.com>
2026-03-05 10:30:30 +0800

    vault backup: 2026-03-05 10:30:30
    Affected files:
    计算机/算法/习题/1758.生成交替二进制字符串的最少操作数.cpp

--- 计算机/算法/习题/1758.生成交替二进制字符串的最少操作数.cpp
+++ 计算机/算法/习题/1758.生成交替二进制字符串的最少操作数.cpp
@@ -13,6 +13,19 @@ public:
     int minOperations(string s) {
         
     }
+    
+    int string0(string s){
+        int count = 0;
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] != '0' && i % 2 == 0 || s[i] != '1' && i % 2 == 1){
+                count++;
+            }
+        }
+    }
+
+    int string1(){
+
+    }
 };
 // @lc code=end
 
