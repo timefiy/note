a1c908fa31c8b901a875c10b8f66c7f13af760c6
timefiy <kw0501199@gmail.com>
2026-03-05 10:25:05 +0800

    vault backup: 2026-03-05 10:25:05
    Affected files:
    计算机/算法/习题/1758.生成交替二进制字符串的最少操作数.cpp
    计算机/算法/习题/未解决/1022. 从根到叶的二进制数之和.md
    计算机/算法/习题/未解决/3713.最长的平衡子串-i.cpp
    计算机/算法/习题/未解决/3714.最长的平衡子串-ii.cpp
    计算机/算法/习题/未解决/3714.最长的平衡子串-ii.md
    计算机/算法/习题/未解决/3721.最长平衡子数组-ii.cpp
    计算机/算法/习题/未解决/assets/1022. 从根到叶的二进制数之和/file-20260305102252521.png
    计算机/算法/习题/未解决/assets/3714.最长的平衡子串-ii/file-20260305102252886.png

---
+++ 计算机/算法/习题/1758.生成交替二进制字符串的最少操作数.cpp
@@ -0,0 +1,18 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=1758 lang=cpp
+ *
+ * [1758] 生成交替二进制字符串的最少操作数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minOperations(string s) {
+        
+    }
+};
+// @lc code=end
+
