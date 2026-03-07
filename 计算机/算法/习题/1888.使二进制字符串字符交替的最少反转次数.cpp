76d775131517754986abd4582b2f2ebc12d2c411
timefiy <kw0501199@gmail.com>
2026-03-07 23:14:21 +0800

    vault backup: 2026-03-07 23:14:20
    Affected files:
    计算机/算法/习题/1888.使二进制字符串字符交替的最少反转次数.cpp

---
+++ 计算机/算法/习题/1888.使二进制字符串字符交替的最少反转次数.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1888 lang=cpp
+ *
+ * [1888] 使二进制字符串字符交替的最少反转次数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minFlips(string s) {
+        
+    }
+};
+// @lc code=end
+
