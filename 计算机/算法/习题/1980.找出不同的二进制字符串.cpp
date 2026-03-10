50b4360b3f87fd7b4e924276747494f28b67693c
timefiy <kw0501199@gmail.com>
2026-03-10 20:07:12 +0800

    vault backup: 2026-03-10 20:07:12
    Affected files:
    计算机/算法/习题/1980.找出不同的二进制字符串.cpp
    计算机/算法/习题/字符串/1888.使二进制字符串字符交替的最少反转次数.cpp
    计算机/算法/习题/字符串/1888.使二进制字符串字符交替的最少反转次数.md
    计算机/算法/习题/字符串/assets/1888.使二进制字符串字符交替的最少反转次数/file-20260308000437063.png

---
+++ 计算机/算法/习题/1980.找出不同的二进制字符串.cpp
@@ -0,0 +1,19 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+
+/*
+ * @lc app=leetcode.cn id=1980 lang=cpp
+ *
+ * [1980] 找出不同的二进制字符串
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        
+    }
+};
+// @lc code=end
+
