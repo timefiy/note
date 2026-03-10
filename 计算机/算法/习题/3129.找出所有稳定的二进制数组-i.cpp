2d474149958d114a6c8dde672ff9c95464c87dd9
timefiy <kw0501199@gmail.com>
2026-03-10 20:47:28 +0800

    vault backup: 2026-03-10 20:47:27
    Affected files:
    计算机/算法/习题/3129.找出所有稳定的二进制数组-i.cpp

---
+++ 计算机/算法/习题/3129.找出所有稳定的二进制数组-i.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=3129 lang=cpp
+ *
+ * [3129] 找出所有稳定的二进制数组 I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numberOfStableArrays(int zero, int one, int limit) {
+        
+    }
+};
+// @lc code=end
+
