7cf1010c0aa32c3a74246ced991581432b76106e
timefiy <kw0501199@gmail.com>
2026-03-11 20:42:37 +0800

    vault backup: 2026-03-11 20:42:36
    Affected files:
    计算机/算法/习题/3129.找出所有稳定的二进制数组-i.cpp

--- 计算机/算法/习题/3129.找出所有稳定的二进制数组-i.cpp
+++ 计算机/算法/习题/3129.找出所有稳定的二进制数组-i.cpp
@@ -7,13 +7,15 @@
 // @lc code=start
 class Solution {
 public:
+    int mod = 1e9 + 7;
+
     int numberOfStableArrays(int zero, int one, int limit) {
         
     }
 
     int dp(int zero, int one, int end, int& limit) {
         if(zero == 0 && one == 0){
-            
+
         }
     }
 };
