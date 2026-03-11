f87a066138786ecc437392fdfa6736f4a996249d
timefiy <kw0501199@gmail.com>
2026-03-11 20:48:28 +0800

    vault backup: 2026-03-11 20:48:27
    Affected files:
    计算机/算法/习题/3129.找出所有稳定的二进制数组-i.cpp

--- 计算机/算法/习题/3129.找出所有稳定的二进制数组-i.cpp
+++ 计算机/算法/习题/3129.找出所有稳定的二进制数组-i.cpp
@@ -1,3 +1,6 @@
+# include <bits/stdc++.h>
+using namespace std;
+
 /*
  * @lc app=leetcode.cn id=3129 lang=cpp
  *
@@ -8,12 +11,24 @@
 class Solution {
 public:
     int mod = 1e9 + 7;
+    vector<vector<int>> memo;
 
     int numberOfStableArrays(int zero, int one, int limit) {
-        
+        memo = vector<vector<int>>(zero + 1, vector<int>(one + 1, -1));
+        return dp(zero, one, -1, limit);
     }
 
     int dp(int zero, int one, int end, int& limit) {
+        if (zero == 0)
+        {
+            return (end == 0 && one <= limit) ? 1 : 0;
+        }
+        if (one == 0)
+        {
+            return (end == 1 && zero <= limit) ? 1 : 0;
+        }
+        
+        
         if(zero == 0 && one == 0){
 
         }
