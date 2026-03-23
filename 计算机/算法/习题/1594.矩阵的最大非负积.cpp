3c0ce2fb9f7749160208f5294c73fc588a9047bf
timefiy <kw0501199@gmail.com>
2026-03-23 20:41:41 +0800

    vault backup: 2026-03-23 20:41:40
    Affected files:
    计算机/算法/习题/1594.矩阵的最大非负积.cpp

--- 计算机/算法/习题/1594.矩阵的最大非负积.cpp
+++ 计算机/算法/习题/1594.矩阵的最大非负积.cpp
@@ -1,8 +1,5 @@
 # include <bits/stdc++.h>
 using namespace std;
-
-const int MOD = 1e9 + 7;
-
 /*
  * @lc app=leetcode.cn id=1594 lang=cpp
  *
@@ -17,26 +14,29 @@ public:
     int maxProductPath(vector<vector<int>>& grid) {
         m = grid.size();
         n = grid[0].size();
-        int res = 1;
         memu = vector<vector<pair<int, int>>>(m, vector<pair<int, int>>(n, {0, 0}));
-        for (int i = 0; i < m; ++i) {
-            for (int j = 0; j < n; ++j) {
-                if (i == 0 && j == 0){
-                    memu[i][j] = {grid[i][j], grid[i][j]};
+
+        for(int i = 0; i < m; ++i){
+            memu[i][0].first = memu[i][0].second = grid[i][0];
+        }
+        for (int j = 0; j < n; j++)
+        {
+            memu[0][j].first = memu[0][j].second = grid[0][j];
+        }
+        
+        for (int i = 1; i < m; ++i) {
+            for (int j = 1; j < n; ++j) {
+                if (grid[i][j] >= 0){
+                    memu[i][j].first = max(memu[i - 1][j].first * grid[i][j], memu[i][j - 1].first * grid[i][j]);
+                    memu[i][j].second = min(memu[i - 1][j].second * grid[i][j], memu[i][j - 1].second * grid[i][j]);
                 }
                 else{
-                    if (grid[i][j] >= 0){
-                        memu[i][j].first = max(memu[i - 1][j].first * grid[i][j], memu[i][j - 1].first * grid[i][j]);
-                        memu[i][j].second = min(memu[i - 1][j].second * grid[i][j], memu[i][j - 1].second * grid[i][j]);
-                    }
-                    else{
-                        memu[i][j].first = max(memu[i - 1][j].second * grid[i][j], memu[i][j - 1].second * grid[i][j]);
-                        memu[i][j].second = min(memu[i - 1][j].first * grid[i][j], memu[i][j - 1].first * grid[i][j]);
-                    }
+                    memu[i][j].first = max(memu[i - 1][j].second * grid[i][j], memu[i][j - 1].second * grid[i][j]);
+                    memu[i][j].second = min(memu[i - 1][j].first * grid[i][j], memu[i][j - 1].first * grid[i][j]);
                 }
             }
         }
-        return memu[m - 1][n - 1].first % MOD;
+        return memu[m - 1][n - 1].first % 100000007;
     }
 };
 // @lc code=end
