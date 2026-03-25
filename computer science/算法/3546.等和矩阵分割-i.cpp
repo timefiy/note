dc9998e236c9b02d6861a8d33ea593602e698813
timefiy <kw0501199@gmail.com>
2026-03-25 22:41:32 +0800

    vault backup: 2026-03-25 22:41:31
    Affected files:
    computer science/算法/3546.等和矩阵分割-i.cpp

--- computer science/算法/3546.等和矩阵分割-i.cpp
+++ computer science/算法/3546.等和矩阵分割-i.cpp
@@ -11,6 +11,16 @@ using namespace std;
 class Solution {
 public:
     bool canPartitionGrid(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<int> rowSum(m), colSum(n);
+        for (int i = 0; i < m; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                rowSum[i] += grid[i][j];
+                colSum[j] += grid[i][j];
+            }
+        }
         
     }
 };
