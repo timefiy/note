d449a4cbf3ae545546d3b7d8ad72d1ccd455027e
timefiy <kw0501199@gmail.com>
2026-03-26 10:43:59 +0800

    vault backup: 2026-03-26 10:43:58
    Affected files:
    computer science/program_language/rust/coding/test1/Cargo.lock
    computer science/program_language/rust/coding/test1/target/.rustc_info.json
    computer science/program_language/rust/coding/test1/target/CACHEDIR.TAG
    computer science/program_language/rust/coding/test1/target/debug/.cargo-lock
    computer science/program_language/rust/coding/test1/target/debug/.fingerprint/test1-2beb2e760b6a0252/invoked.timestamp
    computer science/program_language/rust/coding/test1/target/debug/.fingerprint/test1-2beb2e760b6a0252/output-bin-test1
    computer science/program_language/rust/coding/test1/target/debug/.fingerprint/test1-9beb037e046c023c/invoked.timestamp
    computer science/program_language/rust/coding/test1/target/debug/.fingerprint/test1-9beb037e046c023c/output-test-bin-test1
    computer science/program_language/rust/coding/test1/target/debug/deps/test1-2beb2e760b6a0252.d
    computer science/program_language/rust/coding/test1/target/debug/deps/test1-9beb037e046c023c.d
    computer science/program_language/rust/coding/test1/target/debug/incremental/test1-16367h0ffzxeh/s-hh06thdcog-03kobt7-working/dep-graph.part.bin
    computer science/program_language/rust/coding/test1/target/debug/incremental/test1-16367h0ffzxeh/s-hh06thdcog-03kobt7.lock
    computer science/program_language/rust/coding/test1/target/debug/incremental/test1-271zzppydf947/s-hh06thccfs-0ygc7lk-working/dep-graph.part.bin
    computer science/program_language/rust/coding/test1/target/debug/incremental/test1-271zzppydf947/s-hh06thccfs-0ygc7lk.lock
    computer science/program_language/rust/coding/test1/target/flycheck0/stderr
    computer science/program_language/rust/coding/test1/target/flycheck0/stdout

---
+++ computer science/program_language/rust/coding/test1/target/debug/deps/test1-2beb2e760b6a0252.d
@@ -0,0 +1,5 @@
+E:\note\computer science\program_language\rust\coding\test1\target\debug\deps\test1-2beb2e760b6a0252.d: src\main.rs
+
+E:\note\computer science\program_language\rust\coding\test1\target\debug\deps\libtest1-2beb2e760b6a0252.rmeta: src\main.rs
+
+src\main.rs:
---
+++ computer science/program_language/rust/coding/test1/target/debug/deps/test1-9beb037e046c023c.d
@@ -0,0 +1,5 @@
+E:\note\computer science\program_language\rust\coding\test1\target\debug\deps\test1-9beb037e046c023c.d: src\main.rs
+
+E:\note\computer science\program_language\rust\coding\test1\target\debug\deps\libtest1-9beb037e046c023c.rmeta: src\main.rs
+
+src\main.rs:
