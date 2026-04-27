12c866a37dd6b1aa92cf62c9c1bfb2b435162e87
timefiy <kw0501199@gmail.com>
2026-04-27 15:45:35 +0800

    vault backup: 2026-04-27 15:45:34
    Affected files:
    computer science/algorithm/exercise/1391.检查网格中是否存在有效路径.cpp

---
+++ computer science/algorithm/exercise/1391.检查网格中是否存在有效路径.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=1391 lang=cpp
+ *
+ * [1391] 检查网格中是否存在有效路径
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool hasValidPath(vector<vector<int>>& grid) {
+        
+    }
+};
+// @lc code=end
+
