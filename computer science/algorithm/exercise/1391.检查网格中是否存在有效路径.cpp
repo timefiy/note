0066a459edf7939c466f2871e850f7c5ef4344a3
timefiy <kw0501199@gmail.com>
2026-04-27 15:59:53 +0800

    vault backup: 2026-04-27 15:59:52
    Affected files:
    computer science/algorithm/exercise/1391.检查网格中是否存在有效路径.cpp

--- computer science/algorithm/exercise/1391.检查网格中是否存在有效路径.cpp
+++ computer science/algorithm/exercise/1391.检查网格中是否存在有效路径.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     bool hasValidPath(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
-        vector<pair<int,vector<int>>> dir = {{1,{0,1}},{2,{1,0}}}
+        vector<pair<int,vector<int>>> dir = {{1,{0,1,0,1}},{2,{1,0,1,0}},{3,{0,1,1,0}},{4,{0,-1,1,0}},{5,{1,0,0,-1}},{6,{1,0,0,1}}};
     }
 };
 // @lc code=end
