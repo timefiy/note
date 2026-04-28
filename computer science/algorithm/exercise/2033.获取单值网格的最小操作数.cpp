53b4843e59170c1da8dab07449959690b4bb155f
timefiy <kw0501199@gmail.com>
2026-04-28 20:06:35 +0800

    vault backup: 2026-04-28 20:06:34
    Affected files:
    computer science/algorithm/exercise/2033.获取单值网格的最小操作数.cpp
    computer science/algorithm/exercise/深度优先算法/1391.检查网格中是否存在有效路径.cpp
    computer science/algorithm/exercise/深度优先算法/1391.检查网格中是否存在有效路径.md
    computer science/algorithm/exercise/深度优先算法/assets/1391.检查网格中是否存在有效路径/file-20260428200416513.png

---
+++ computer science/algorithm/exercise/2033.获取单值网格的最小操作数.cpp
@@ -0,0 +1,18 @@
+# include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=2033 lang=cpp
+ *
+ * [2033] 获取单值网格的最小操作数
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minOperations(vector<vector<int>>& grid, int x) {
+        
+    }
+};
+// @lc code=end
+
