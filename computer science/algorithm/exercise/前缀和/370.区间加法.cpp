753b6d77100c27475b3a62b64fddfb493f96ceaf
timefiy <kw0501199@gmail.com>
2026-05-13 19:41:06 +0800

    vault backup: 2026-05-13 19:41:05
    Affected files:
    computer science/algorithm/exercise/前缀和/370.区间加法.cpp
    computer science/algorithm/第一章 经典数据结构算法/手把手刷数组算法/小而美的算法技巧：差分数组.md

--- computer science/algorithm/exercise/前缀和/370.区间加法.cpp
+++ computer science/algorithm/exercise/前缀和/370.区间加法.cpp
@@ -2,6 +2,53 @@
 
 using namespace std;
 
-int main(){
+class Solution {
+
+public:
+
+    vector<int> getModifiedArray(int length, vector<vector<int>>& updates) {
+        // nums 初始化为全 0
+        vector<int> nums(length, 0);
+        // 构造差分解法
+        Difference df(nums);
+        for (const auto& update : updates) {
+            int i = update[0];
+            int j = update[1];
+            int val = update[2];
+            df.increment(i, j, val);
+        }
+        return df.result();
+    }
     
-}
+    class Difference {
+        // 差分数组
+        vector<int> diff;
+    public:
+        Difference(const vector<int>& nums) {
+            assert(!nums.empty());
+            diff.resize(nums.size());
+            // 构造差分数组
+            diff[0] = nums[0];
+            for (size_t i = 1; i < nums.size(); ++i) {
+                diff[i] = nums[i] - nums[i - 1];
+            }
+        }
+        // 给闭区间 [i, j] 增加 val（可以是负数）
+        void increment(int i, int j, int val) {
+            diff[i] += val;
+            if (j + 1 < diff.size()) {
+                diff[j + 1] -= val;
+            }
+        }
+        vector<int> result() {
+            vector<int> res(diff.size());
+            // 根据差分数组构造结果数组
+            res[0] = diff[0];
+            for (size_t i = 1; i < diff.size(); ++i) {
+                res[i] = res[i - 1] + diff[i];
+            }
+            return res;
+        }
+    };
+
+};
