58b0c31218c9ca12e3ce2c2c4794e29e3c68d9fe
timefiy <kw0501199@gmail.com>
2026-05-14 11:08:49 +0800

    vault backup: 2026-05-14 11:08:48
    Affected files:
    computer science/algorithm/exercise/2784.检查数组是否是好的.cpp

---
+++ computer science/algorithm/exercise/2784.检查数组是否是好的.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+ * @lc app=leetcode.cn id=2784 lang=cpp
+ *
+ * [2784] 检查数组是否是好的
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool isGood(vector<int>& nums) {
+        
+    }
+};
+// @lc code=end
+
