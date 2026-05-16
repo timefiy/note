8c5e71849fa6afa6943fba96a10f6b15e3b7a374
timefiy <kw0501199@gmail.com>
2026-05-16 18:03:37 +0800

    vault backup: 2026-05-16 18:03:37
    Affected files:
    .codex/skills/algorithm-md-writer/SKILL.md
    computer science/algorithm/exercise/153.寻找旋转排序数组中的最小值.cpp
    computer science/algorithm/exercise/二分查找/153.寻找旋转排序数组中的最小值.md

--- computer science/algorithm/exercise/153.寻找旋转排序数组中的最小值.cpp
+++ computer science/algorithm/exercise/153.寻找旋转排序数组中的最小值.cpp
@@ -14,20 +14,16 @@ public:
         int left = 0;
         int right = nums.size() - 1;
 
-        // 当 left == right 时，区间缩小为一个点，这就是我们要找的最小值
         while (left < right) {
-            int mid = left + (right - left) / 2; // 防止整数溢出
+            int mid = left + (right - left) / 2; 
 
             if (nums[mid] > nums[right]) {
-                // 最小值在 mid 的右侧
                 left = mid + 1;
             } else {
-                // 最小值在 mid 或 mid 的左侧
                 right = mid;
             }
         }
 
-        // 循环结束时 left == right，指向的就是最小值
         return nums[left];
     }
 };
