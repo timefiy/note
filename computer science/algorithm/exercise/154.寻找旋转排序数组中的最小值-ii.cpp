8b8fbdd85d1dd277bfc72d7a5f57d08fc2e04556
timefiy <kw0501199@gmail.com>
2026-05-16 19:28:25 +0800

    vault backup: 2026-05-16 19:28:21
    Affected files:
    computer science/algorithm/exercise/154.寻找旋转排序数组中的最小值-ii.cpp

--- computer science/algorithm/exercise/154.寻找旋转排序数组中的最小值-ii.cpp
+++ computer science/algorithm/exercise/154.寻找旋转排序数组中的最小值-ii.cpp
@@ -14,30 +14,13 @@ public:
         int left = 0;
         int right = nums.size() - 1;
 
-        while (left < right) {
-            int mid = left + (right - left) / 2; 
+        while (left < right)
+        {
+            int mid = left + (right - left) / 2;
 
-            if (nums[mid] > nums[mid + 1]) {
-                left = mid + 1;
-            } if (nums[mid] == nums[mid + 1]){
-                for (int i = 1; i < nums.size()/2; i++)
-                {
-                    if (nums[mid + i] < nums[mid]) {
-                        return nums[mid + i];
-                        
-                    }
-                    if (nums[mid - i] < nums[mid]) {
-                        right = mid;
-                    }
-                }
-            } 
-            else {
-                right = mid;
-            }
+            
         }
-
-        return nums[left];
-    }
+        
 };
 // @lc code=end
 
