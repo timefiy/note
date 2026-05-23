7c0a3420ef9947bec61d1f92b85df7d9a59d1399
timefiy <kw0501199@gmail.com>
2026-05-23 22:14:42 +0800

    vault backup: 2026-05-23 22:14:41
    Affected files:
    computer science/algorithm/exercise/1752.检查数组是否经排序和轮转得到.cpp

--- computer science/algorithm/exercise/1752.检查数组是否经排序和轮转得到.cpp
+++ computer science/algorithm/exercise/1752.检查数组是否经排序和轮转得到.cpp
@@ -11,27 +11,21 @@ using namespace std;
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        vector<int> nums2 = nums;
-        sort(nums2.begin(), nums2.end());
-
-        int x;
-        int a = nums[0];
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums[i] == a)
-            {
-                x = i;
-                break;
+        int count = 0;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            // 用模运算模拟环形数组：当 i = n-1 时，(i + 1) % n 会自动回到 0
+            if (nums[i] > nums[(i + 1) % n]) {
+                count++;
             }
-        }
-
-        for (int i = 0; i < nums.size(); i++)
-        {
-            if (nums2[i+x] != nums[(i) % nums.size()])
-            {
+            
+            // 剪枝优化：如果在中途发现下降次数已经超过 1 次，直接断定不合法
+            if (count > 1) {
                 return false;
             }
         }
+        
         return true;
     }
 };
