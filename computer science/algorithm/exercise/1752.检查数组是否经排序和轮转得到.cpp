114340d3150ac1f242dbc3a74d2af84c70752fec
timefiy <kw0501199@gmail.com>
2026-05-23 22:10:54 +0800

    vault backup: 2026-05-23 22:10:53
    Affected files:
    computer science/algorithm/exercise/1752.检查数组是否经排序和轮转得到.cpp

--- computer science/algorithm/exercise/1752.检查数组是否经排序和轮转得到.cpp
+++ computer science/algorithm/exercise/1752.检查数组是否经排序和轮转得到.cpp
@@ -13,6 +13,26 @@ public:
     bool check(vector<int>& nums) {
         vector<int> nums2 = nums;
         sort(nums2.begin(), nums2.end());
+
+        int x;
+        int a = nums[0];
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == a)
+            {
+                x = i;
+                break;
+            }
+        }
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (nums2[i+x] != nums[(i) % nums.size()])
+            {
+                return false;
+            }
+        }
+        return true;
     }
 };
 // @lc code=end
