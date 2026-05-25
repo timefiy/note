255be732ef84f287f596bbe4d1b058b9aafa7bc3
timefiy <kw0501199@gmail.com>
2026-05-25 23:13:51 +0800

    vault backup: 2026-05-25 23:13:50
    Affected files:
    computer science/algorithm/exercise/1871.跳跃游戏-vii.cpp
    computer science/algorithm/exercise/数组/环形数组/1752.检查数组是否经排序和轮转得到.md
    computer science/algorithm/exercise/数组/环形数组/assets/1752.检查数组是否经排序和轮转得到/file-20260525231218871.png

---
+++ computer science/algorithm/exercise/1871.跳跃游戏-vii.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1871 lang=cpp
+ *
+ * [1871] 跳跃游戏 VII
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool canReach(string s, int minJump, int maxJump) {
+        
+    }
+};
+// @lc code=end
+
