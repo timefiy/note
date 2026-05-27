fc3580aca490b9223ec596e56fe596d43e7aebe3
timefiy <kw0501199@gmail.com>
2026-05-27 23:55:41 +0800

    vault backup: 2026-05-27 23:55:40
    Affected files:
    computer science/algorithm/exercise/3120.统计特殊字母的数量-i.cpp
    computer science/algorithm/exercise/前缀和/1871.跳跃游戏-vii.cpp
    computer science/algorithm/exercise/前缀和/1871.跳跃游戏-vii.md

---
+++ computer science/algorithm/exercise/3120.统计特殊字母的数量-i.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+ * @lc app=leetcode.cn id=3120 lang=cpp
+ *
+ * [3120] 统计特殊字母的数量 I
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numberOfSpecialChars(string word) {
+        
+    }
+};
+// @lc code=end
+
