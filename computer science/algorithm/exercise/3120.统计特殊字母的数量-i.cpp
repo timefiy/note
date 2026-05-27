70fe3fbc28001b1e61a24fd88b5869b69764add6
timefiy <kw0501199@gmail.com>
2026-05-27 23:59:29 +0800

    vault backup: 2026-05-27 23:59:28
    Affected files:
    computer science/algorithm/exercise/3120.统计特殊字母的数量-i.cpp

--- computer science/algorithm/exercise/3120.统计特殊字母的数量-i.cpp
+++ computer science/algorithm/exercise/3120.统计特殊字母的数量-i.cpp
@@ -15,10 +15,28 @@ public:
 
         vector<int> count(26, 0);
         for (char c : word) {
-            if (c >= 'a' && c <= 'z' && count[c - 'a'] < 2) {
-                count[c - 'a'];
+            if (c >= 'a' && c <= 'z') {
+                if(count[c - 'a'] == 2){
+                    ans++;
+                    count[c - 'a'] = 3;
+                }
+
+                if(count[c - 'a'] != 3){
+                    count[c - 'a'] = 1;
+                }
+            }
+            if (c >= 'A' && c <= 'Z') {
+                if(count[c - 'A'] == 1){
+                    ans++;
+                    count[c - 'A'] = 3;
+                }
+
+                if(count[c - 'A'] != 3){
+                    count[c - 'A'] = 2;
+                }
             }
         }
+        return ans;
     }
 };
 // @lc code=end
